8b87463955c1302d7e1aed09ea40f975c50290a6
Ryan McIntire <ryanpmcintire@gmail.com>
2024-02-22 15:33:35 -0500

    tictactoe

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+function Square({ value,onSquareClick }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button
+      className="square"
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+function Board({ xIsNext,squares,onPlay }) {
+  function handleClick(i) {
+    if(calculateWinner(squares) || squares[i]) return;
+
+    const nextSquares = squares.slice();
+
+    if(xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner) {
+    status = `Winner is: ${winner}`;
+  } else {
+    status = `Next player is: ${xIsNext ? "X" : "O"}`;
+  }
+
+  return (
+    <>
+      <div className="status">
+        {status}
+      </div>
+
+      {populateBoard()}
+    </>
+  );
+
+  function populateBoard() {
+    return [...Array(3)].map((_,rowOffset) => {
+      return (
+        <div key={rowOffset} className="board-row">
+          {populateRow(rowOffset * 3)}
+        </div>
+      )
+    })
+  }
+
+  function populateRow(rowOffset) {
+    return [...Array(3)].map((_,i) => {
+      return <Square key={i + rowOffset} value={squares[i + rowOffset]} onSquareClick={() => handleClick(i + rowOffset)} />;
+    });
+  }
+}
+
+export default function Game() {
+  const [history,setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove,setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0,currentMove + 1),nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares,move) => {
+    let description;
+
+    if(move > 0) {
+      description = `Go to move #${move}`;
+    } else {
+      description = 'Go to game start';
+    }
+
+    const goToMoveButton = <button onClick={() => jumpTo(move)}>{description}</button>;
+    const currentMoveDisplay = <span>You are at move #{move}</span>;
+
+    return (
+      <ul key={move}>
+        {move === currentMove ? currentMoveDisplay : goToMoveButton}
+      </ul>
+    )
+  })
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
     </div>
   );
 }
 
-export default App;
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+  for(let i = 0; i < lines.length; i++) {
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
